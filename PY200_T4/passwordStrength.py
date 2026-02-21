36e37000486af4d98fa739d5ccadd3222856c42b
coderujwal3 <nikhilsengar7012@gmail.com>
2026-02-21 18:34:25 +0530

    Upgraded solution of PY200_T4 Password Strength Checker

--- PY200_T4/passwordStrength.py
+++ PY200_T4/passwordStrength.py
@@ -7,14 +7,24 @@
 """
 
 
+
+print(f"{"-"*30}\nFor Valid password your password should be -\n\t- at least 8 characters long\n\t- 1 uppercase character\n\t- 1 lowercase character\n\t- and 1 digit character\n{"-"*30}\n")
 inp = input("Enter the password: ")
 
 if len(inp) >= 8:
-    if(any(i.isdigit() for i in inp) and any(i.isupper() for i in inp) and any(i.islower() for i in inp)):
-        print("Password is valid (STRONG)")
+    if(any(i.isdigit() for i in inp)):
+        if(any(i.isupper() for i in inp)):
+            if(any(i.islower() for i in inp)):
+               print("Password is valid (STRONG)")
+            else:
+                print("Password is not valid (WEAK)")
+                print("Password must have 1 lowercase character")
+        else:
+            print("Password is not valid (WEAK)")
+            print("Password must have 1 uppercase character")
     else:
         print("Password is not valid (WEAK)")
-        print("For Valid password your password should be -\n\tat least 8 characters long\n\t1 uppercase character\n\t1 lowercase character\n\tand 1 digit\n")
+        print("Password must have 1 digit")
 else:
     print("\nPassword is WEAK")
-    print("For Valid password your password should be -\n\tat least 8 characters long\n\t1 uppercase character\n\t1 lowercase character\n\tand 1 digit\n")
+    print("Password must be 8 characters long")
