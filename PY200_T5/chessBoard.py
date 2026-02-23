0ef671840484bf654b0634c1d8e963d07d674c33
coderujwal3 <nikhilsengar7012@gmail.com>
2026-02-23 20:42:57 +0530

    Solution of PY200_T5 Chessboard

--- PY200_T5/chessBoard.py
+++ PY200_T5/chessBoard.py
@@ -3,13 +3,24 @@
     '0' for odd, and 'X' on the main diagonal.
     Rows and columns are 0-indexed in this example.
 """
+
+# taking user input - in integer format
 inp = int(input("Enter a number: "))
+
+# loop for rows
 for row in range(inp):
+    # loop for columns
     for col in range(inp):
+        # when row and columns is same - diagonals (you can analyze it by using any matrix) (because chessboard is like a matrix)
         if row == col:
             print('X', end=' ')
+        
+        # if it is not diagonal - check the sum of rowValue and columnValue is even (if it is then print '1 ')
         elif (row + col) % 2 == 0:
             print('1', end=' ')
+        
+        # if it is not diagonal, neither the sum of row and column is even, then just print '0 '
         else:
             print('0', end=' ')
+
     print() # Move to the next line after each row
