4f39cb5742e7433c6e557e66e4985dc0d01ec4b8
coderujwal3 <nikhilsengar7012@gmail.com>
2026-02-23 21:07:35 +0530

    Solution of PY200_T6 digit constraints

--- PY200_T6/digitConstraints.py
+++ PY200_T6/digitConstraints.py
@@ -4,24 +4,37 @@
     - x contains exactly two 3s
     - x does not contain digit 0
 """
+
+# taking the user input
 inp = int(input("Enter a number: "))
+# using this flag later on for checking that there is any element or not which satisfies the condition
 flag = 0
 
+# iterating from 1 to input's number +1 (input number of times) (if inp = 5 then 5 times)
 for x in range(1, inp + 1):
+    
+    # Convert x to a string
     s = str(x)
+
     # Check if x contains digit 0
     if '0' in s:
-        continue
+        continue    # jump the iteration, when hitting this condition
 
     # Check if x contains exactly two 3s
     if s.count('3') != 2:
-        continue
+        continue    # jump the iteration, when hitting this condition
 
+    # Initialize the total sum to zero
+    digit_sum = 0
+    # iterating through string 's' and add the all integers in that string one-by-one
+    for digit in s:
+        digit_sum += int(digit)
+    
     # Check if the sum of digits of x is divisible by 7
-    digit_sum = sum(int(digit) for digit in s)
     if digit_sum % 7 == 0:
-        flag = 1
+        flag = 1        # if any 1 answer is also found, then make the flag 1 (true) - which means that there is any final answer exist, we don't have to print -1
         print(x)
 
+# when there is no value existing in the range, then print '-1'
 if (flag == 0):
-    print("-1")
+    print(-1)
